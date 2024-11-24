0c90e0abeffc67e9f1ec91bbf3bcf9d9aafc032c
anshitaagarg <0548anshita@gmail.com>
2024-11-24 14:15:20 +0530

    Update QuotientWithException .java

--- QuotientWithException .java
+++ QuotientWithException .java
@@ -2,27 +2,25 @@ import java.util.*;
 //ExceptionHandling
  public class QuotientWithException {
  public static double quotient(double number1, double number2) {
- if (number2 == 0)
- throw new ArithmeticException("Divisor cannot be zero");
+ // if (number2 == 0)
+ // throw new ArithmeticException("Divisor cannot be zero");
  return number1 / number2;
  }
    
  public static void main(String[] args) {
  Scanner input = new Scanner(System.in);
-
  // Prompt the user to enter two integers
  System.out.print("Enter two integers: ");
  double number1 = input.nextDouble();
  double number2 = input.nextDouble();
-//try and catch block
+ //try and catch block
  try {
  double result = quotient(number1, number2);
  System.out.println(number1 + " / " + number2 + " is " + result);
  }
  catch (ArithmeticException ex) {
- System.out.println("Exception: an integer " + "cannot be divided by zero ");
+ System.out.println("Exception: an integer cannot be divided by zero ");
  }
-
  System.out.println("Execution continues ...");
  }
-}
+ }
